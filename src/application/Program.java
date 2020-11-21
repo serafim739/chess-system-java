ee7ae5b6a59a5da4ce8208ec126120dc51ae07d9
serafim739 <nyvalldo_shekina@hotmail.com.br>
2020-11-21 18:11:15 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 				if(captured != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			} 
 			
 			catch (ChessException e) {
